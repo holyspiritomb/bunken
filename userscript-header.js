c525e1f02e9f123ac7bea225d7df87ca5a134f8c
Hezekiah Michael <pianycist@gmail.com>
2022-08-18 22:05:18 -0400

    initial commit of userscript header

---
+++ userscript-header.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Bunken by laxyapahuja, as a userscript
+// @namespace   https://github.com/holyspiritomb
+// @match       https://www.goodreads.com/book/show/*
+// @grant       none
+// @version     0.0.1
+// @author      holyspiritomb
+// @description Search libgen, memoryoftheworld, audiobookbay, and openlibrary from Goodreads! Forked from https://github.com/laxyapahuja/bunken for use as a userscript.
+// @require     https://greasyfork.org/scripts/446668-jquery-core-slim-minified/code/jQuery%20Core%20slim%20minified.js?version=1062206
+// ==/UserScript==
