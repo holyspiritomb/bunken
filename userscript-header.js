5c6a0932fcf9fa6abec2dc5e46fe593821203d05
Hezekiah Michael <pianycist@gmail.com>
2022-08-19 19:58:16 -0400

    add run-at: document-idle

--- userscript-header.js
+++ userscript-header.js
@@ -5,6 +5,7 @@
 // @grant       none
 // @version     0.0.1
 // @author      holyspiritomb
-// @description Search libgen, memoryoftheworld, audiobookbay, and openlibrary from Goodreads, via a box under book descriptions. Forked from https://github.com/laxyapahuja/bunken for use as a userscript.
+// @description Search libgen, memoryoftheworld, audiobookbay, and openlibrary from Goodreads. Forked from https://github.com/laxyapahuja/bunken for use as a userscript.
 // @require     https://greasyfork.org/scripts/446668-jquery-core-slim-minified/code/jQuery%20Core%20slim%20minified.js?version=1062206
+// @run-at      document-idle
 // ==/UserScript==
