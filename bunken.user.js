c215b5036fc6020297db3a0dad6f756219b3a217
Hezekiah Michael <pianycist@gmail.com>
2022-10-28 14:58:31 -0400

    add detection of old layouts

--- bunken.user.js
+++ bunken.user.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name        Bunken Forked
+// @namespace   https://github.com/holyspiritomb
+// @match       https://www.goodreads.com/book/show/*
+// @website     https://github.com/holyspiritomb/bunken
+// @grant       none
+// @version     0.0.1
+// @author      holyspiritomb
+// @description Search libgen, memoryoftheworld, audiobookbay, and openlibrary from Goodreads. Forked from https://github.com/laxyapahuja/bunken for use as a userscript.
+// @require     https://greasyfork.org/scripts/446668-jquery-core-slim-minified/code/jQuery%20Core%20slim%20minified.js?version=1062206
+// @run-at      document-idle
+// ==/UserScript==
 const API = 'https://api.bunken.tk/'
 
 let isRedesign = !document.querySelector("[property='books:isbn']")
