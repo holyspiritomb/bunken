d838e49f7da6d3cc2914495e1ac373105fc81422
Hezekiah Michael <pianycist@gmail.com>
2022-10-28 14:59:47 -0400

    add version number bump

--- userscript-header.js
+++ userscript-header.js
@@ -4,7 +4,7 @@
 // @match       https://www.goodreads.com/book/show/*
 // @website     https://github.com/holyspiritomb/bunken
 // @grant       none
-// @version     0.0.1
+// @version     0.0.2
 // @author      holyspiritomb
 // @description Search libgen, memoryoftheworld, audiobookbay, and openlibrary from Goodreads. Forked from https://github.com/laxyapahuja/bunken for use as a userscript.
 // @require     https://greasyfork.org/scripts/446668-jquery-core-slim-minified/code/jQuery%20Core%20slim%20minified.js?version=1062206
