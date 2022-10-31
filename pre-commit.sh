9608094d27ec0034dcbe9deeaaf47c238acc1667
Hezekiah Michael <spiritomb@protonmail.com>
2022-10-30 21:37:54 -0400

    move pre-commit script

---
+++ pre-commit.sh
@@ -0,0 +1,7 @@
+cat userscript-header.js support/goodreads/script.js > bunken.user.js
+git add bunken.user.js
+head -n 5 userscript-header.js > bunken-adguard.user.js
+echo '// @grant       unsafeWindow' >> bunken-adguard.user.js
+tail -n 6 userscript-header.js >> bunken-adguard.user.js
+cat support/goodreads/script.js >> bunken-adguard.user.js
+git add bunken-adguard.user.js
