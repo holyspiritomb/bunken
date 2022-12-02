79f86b994681b198060ee77c7fa5135f54028ed9
Hezekiah Michael <github.cupel@aleeas.com>
2022-12-01 22:54:45 -0500

    version increment

--- userscript-header.js
+++ userscript-header.js
@@ -4,7 +4,7 @@
 // @match       https://www.goodreads.com/book/show/*
 // @website     https://github.com/holyspiritomb/bunken/tree/userscript
 // @grant       none
-// @version     0.0.2
+// @version     0.1.0
 // @author      holyspiritomb
 // @downloadURL https://raw.githubusercontent.com/holyspiritomb/bunken/userscript/bunken.user.js
 // @description Search libgen, memoryoftheworld, audiobookbay, and openlibrary from Goodreads. Forked from https://github.com/laxyapahuja/bunken for use as a userscript.
