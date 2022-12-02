068c03aab57997afffb5c4016aa99c383d83d04b
Hezekiah Michael <pianycist@gmail.com>
2022-12-01 23:05:39 -0500

    modify build scriot

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,6 @@
 cat userscript-header.js support/goodreads/script.js > bunken.user.js
 git add bunken.user.js
-head -n 5 userscript-header.js > bunken-adguard.user.js
-echo '// @grant       unsafeWindow' >> bunken-adguard.user.js
-tail -n 6 userscript-header.js >> bunken-adguard.user.js
-cat support/goodreads/script.js >> bunken-adguard.user.js
+cat userscript-header.js support/goodreads/script.js > bunken-adguard.user.js
+sed -i 's/grant       none/grant       unsafeWindow/g' bunken-adguard.user.js
 sed -i "s/bunken.user.js/bunken-adguard.user.js/g" bunken-adguard.user.js
 git add bunken-adguard.user.js
