a0d685802ebaeb27c14587788f4a90cb6141b43a
Hezekiah Michael <pianycist@gmail.com>
2022-12-01 23:08:09 -0500

    update

--- bunken-adguard.user.js
+++ bunken-adguard.user.js
@@ -4,8 +4,9 @@
 // @match       https://www.goodreads.com/book/show/*
 // @website     https://github.com/holyspiritomb/bunken/tree/userscript
 // @grant       unsafeWindow
+// @version     0.1.0
 // @author      holyspiritomb
-// @downloadURL https://raw.githubusercontent.com/holyspiritomb/bunken/userscript/bunken.user.js
+// @downloadURL https://raw.githubusercontent.com/holyspiritomb/bunken/userscript/bunken-adguard.user.js
 // @description Search libgen, memoryoftheworld, audiobookbay, and openlibrary from Goodreads. Forked from https://github.com/laxyapahuja/bunken for use as a userscript.
 // @require     https://greasyfork.org/scripts/446668-jquery-core-slim-minified/code/jQuery%20Core%20slim%20minified.js?version=1062206
 // @run-at      document-idle
